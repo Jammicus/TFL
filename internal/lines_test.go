5dd5068eae400d6150f6648fc64efc066c72bb90
James Walter <jamesmaxwalter@gmail.com>
2020-01-25 18:29:03 +0000

    Fix tests

--- internal/lines_test.go
+++ internal/lines_test.go
@@ -1,22 +1,9 @@
 package api
 
 import (
+	"github.com/google/go-cmp/cmp"
 	"testing"
 )
-
-var testCases = []Lines{
-	{"bakerloo", "Bakerloo", "tube", "", testCasesLineStatus},
-	{"central", "Central", "tube", "", testCasesLineStatus},
-	{"circle", "Circle", "tube", "", testCasesLineStatus},
-	{"district", "District", "tube", "", testCasesLineStatus},
-	{"hammersmith-city", "Hammersmith & City", "tube", "", testCasesLineStatus},
-	{"jubilee", "Jubilee", "tube", "", testCasesLineStatus},
-	{"metropolitan", "Metropolitan", "tube", "", testCasesLineStatus},
-	{"northern", "Northern", "tube", "", testCasesLineStatus},
-	{"piccadilly", "Piccadilly", "tube", "", testCasesLineStatus},
-	{"victoria", "Victoria", "tube", "", testCasesLineStatus},
-	{"waterloo-city", "Waterloo & City", "tube", "", testCasesLineStatus},
-}
 
 var testCasesLineStatus = []lineStatus{
 	{0, "", 10, "Good Service", "", []validityPeriods{}},
@@ -25,15 +12,32 @@ var testCasesLineStatus = []lineStatus{
 
 func TestDetermineStatus(t *testing.T) {
 	var testCases = []struct {
-		input    testCases
+		input    []Lines
 		expected []LineSummary
 	}{
-		{testCases[0], []LineSummary{}},
+		{[]Lines{Lines{"bakerloo", "Bakerloo", "tube", "", testCasesLineStatus}}, []LineSummary{LineSummary{"Bakerloo", "Good Service", ""}}},
+		{[]Lines{Lines{"central", "Central", "tube", "", testCasesLineStatus}}, []LineSummary{LineSummary{"Central", "Good Service", ""}}},
+		{[]Lines{Lines{"circle", "Circle", "tube", "", testCasesLineStatus}}, []LineSummary{LineSummary{"Circle", "Good Service", ""}}},
+		{[]Lines{Lines{"district", "District", "tube", "", testCasesLineStatus}}, []LineSummary{LineSummary{"District", "Good Service", ""}}},
+		{[]Lines{Lines{"hammersmith-city", "Hammersmith & City", "tube", "", testCasesLineStatus}}, []LineSummary{LineSummary{"Hammersmith & City", "Good Service", ""}}},
+		{[]Lines{Lines{"jubilee", "Jubilee", "tube", "", testCasesLineStatus}}, []LineSummary{LineSummary{"Jubilee", "Good Service", ""}}},
+		{[]Lines{Lines{"metropolitan", "Metropolitan", "tube", "", testCasesLineStatus}}, []LineSummary{LineSummary{"Metropolitan", "Good Service", ""}}},
+		{[]Lines{Lines{"northern", "Northern", "tube", "", testCasesLineStatus}}, []LineSummary{LineSummary{"Northern", "Good Service", ""}}},
+		{[]Lines{Lines{"piccadilly", "Piccadilly", "tube", "", testCasesLineStatus}}, []LineSummary{LineSummary{"Piccadilly", "Good Service", ""}}},
+		{[]Lines{Lines{"victoria", "Victoria", "tube", "", testCasesLineStatus}}, []LineSummary{LineSummary{"Victoria", "Good Service", ""}}},
+		{[]Lines{Lines{"waterloo-city", "Waterloo & City", "tube", "", testCasesLineStatus}}, []LineSummary{LineSummary{"Waterloo & City", "Good Service", ""}}},
 	}
 
 	for _, test := range testCases {
-		if item, _ := DetermineStatus(test.input, test.expected); item != test.expected {
-			t.Error("Expected: ", test.expected, "But got: ", item)
+
+		output := DetermineStatus(test.input)
+
+		for _, item := range output {
+
+			if !cmp.Equal(item, test.expected[0]) {
+				t.Error("Expected: ", test.expected, "But got: ", item)
+			}
 		}
+
 	}
 }
